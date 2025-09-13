fc4e8f129c98641966173941cdf55209cf59bf47
facundoduhau <duhaufacundo@gmail.com>
2025-09-12 23:16:48 -0300

    Added files for tomorrow

---
+++ Programming/Udemy/WebDev/AdvancedJS/promises.js
@@ -0,0 +1,19 @@
+function fetchData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            let success = true;
+            if(success){
+                resolve("Data fetched with no problems")
+            }
+            else{
+                reject("There was a problem fetching the data")
+            }
+        }, 3000)
+    })
+}
+
+var response = fetchData();
+
+console.log(response);
+
+// To know the fetched data status, you can do:
