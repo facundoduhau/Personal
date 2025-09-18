84e03b73a9269512256176680f44077d3dfc3a5b
facundoduhau <duhaufacundo@gmail.com>
2025-09-18 20:37:42 -0300

    Finished async

---
+++ Programming/Udemy/WebDev/AdvancedJS/async-await-promises-part2.js
@@ -0,0 +1,15 @@
+function fetchPostData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("Post data fetched")
+        }, 3000);
+    });
+};
+
+function fetchCommentData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("Comment data fecthed with no errors")
+        }, 6000);
+    });
+}
---
+++ Programming/Udemy/WebDev/AdvancedJS/async-await-promises.js
@@ -0,0 +1,25 @@
+function fetchUserData(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve({name: 'Facundo', url: 'https://facundowebdev.com'})
+        }, 3000)
+    })
+};
+
+fetchUserData // Traditional method
+    .then()
+    .catch();
+
+async function getUserData() {
+    try{
+        console.log("Fetching user data...");
+        console.log("User data fetched succesfully");
+        const userData = await fetchUserData() // await only works with async
+        console.log("User data: ", userData);
+    }
+    catch (error){
+        console.log("Error fetching data", error);
+    }
+}
+
+// You can send data as rejected by adding literally "reject" before the data
